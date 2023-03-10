eef8b3b9b6e9f9a0ee20cfeaa5dd9bc73b3c24aa
haetsal <leeee99@hanmail.net>
2023-03-10 18:00:27 +0900

    Feat : 내브바 레이아웃 작성

--- front/src/pages/DebuggingPage.tsx
+++ front/src/pages/DebuggingPage.tsx
@@ -3,15 +3,11 @@ import styled from 'styled-components';
 import Header from '../components/ui/Header';
 import { useNavigate } from 'react-router-dom';
 
-const Wrapper = styled.div`
-  padding-top: 80px;
-`;
-
 function DebuggingPage() {
   const navigate = useNavigate();
 
   return (
-    <Wrapper>
+    <>
       <Header />
       <h1>디버깅 페이지</h1>
       <button onClick={() => navigate('/owstore/0')}>사장님-가게</button>
@@ -22,7 +18,7 @@ function DebuggingPage() {
         사장님-가게리스트
       </button>
       <button onClick={() => navigate('/owregister')}>가맹점등록</button>
-    </Wrapper>
+    </>
   );
 }
 export default DebuggingPage;
--- front/src/pages/owner/StoreRegisterPage.tsx
+++ front/src/pages/owner/StoreRegisterPage.tsx
@@ -3,17 +3,20 @@ import styled from 'styled-components';
 import Header from '../../components/ui/Header';
 import MainComponent from '../../components/ui/MainComponent';
 
-const Wrapper = styled.div`
-  padding-top: 80px;
+const ContentsStyle = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 function StoreRegisterPage() {
   return (
-    <Wrapper>
+    <>
       <Header />
-      <h1>StoreRegisterPage</h1>
-      <MainComponent width={80} />
-    </Wrapper>
+      <ContentsStyle>
+        <h1>StoreRegisterPage</h1>
+        <MainComponent width={80} />
+      </ContentsStyle>
+    </>
   );
 }
 export default StoreRegisterPage;
