27a6ce6f6df06192201a2ecf2a002c12056b3a00
hn99515 <hn99515@gmail.com>
2023-03-10 13:54:59 +0900

    Feat : Header 추가

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../components/ui/Header';
+
+const Wrapper = styled.div`
+  padding-top: 80px;
+`;
 
 const MainPage: React.FC = () => {
   return (
-    <div>
-        <h1>메인 페이지</h1>
-    </div>
-  )
-}
+    <Wrapper>
+      <Header />
+      <h1>메인 페이지</h1>
+    </Wrapper>
+  );
+};
 export default MainPage;
