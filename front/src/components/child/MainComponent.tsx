a0e48ef7fbf690d7a380c6bb3774e2b516a502cc
jungeun park <pyooooong@gmail.com>
2023-03-13 15:53:34 +0900

    Feat : 사이드바 메뉴 클릭에 따른 메인 컴포넌트 변경 구현

---
+++ front/src/components/child/MainComponent.tsx
@@ -0,0 +1,8 @@
+
+const MainComponent = () => {
+    return (
+        <h1>아동 메인</h1>
+    )
+}
+
+export default MainComponent;
---
+++ front/src/components/child/Setting.tsx
@@ -0,0 +1,9 @@
+
+
+const Settings = () => {
+    return (
+        <h1>아동 세팅</h1>
+    )
+}
+
+export default Settings;
