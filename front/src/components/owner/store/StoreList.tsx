897d4d74036b369693a0958f4d8d7d7b5ec57897
haetsal <leeee99@hanmail.net>
2023-03-14 17:19:13 +0900

    Feat : 가맹점 목록 출력

--- front/src/components/owner/store/StoreList.tsx
+++ front/src/components/owner/store/StoreList.tsx
@@ -1,4 +1,33 @@
+import styled from 'styled-components';
+import StoreListItem from './StoreListItem';
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
 function StoreList() {
-  return <h1>가맹점 목록</h1>;
+  const stores = [
+    {
+      id: 1,
+      storeName: '스테이크 팩토리1',
+      storeText: '항상 후원',
+    },
+    {
+      id: 2,
+      storeName: '스테이크 팩토리2',
+      storeText: '항상 후원',
+    },
+    {
+      id: 3,
+      storeName: '스테이크 팩토리3',
+      storeText: '항상 후원',
+    },
+  ];
+  return (
+    <Wrapper>
+      <h1>가맹점 목록</h1>
+      <StoreListItem stores={stores} />
+    </Wrapper>
+  );
 }
 export default StoreList;
