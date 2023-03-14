ded16195f76f88f37f82c3eb577a4acdb30d8015
hn99515 <hn99515@gmail.com>
2023-03-14 10:20:52 +0900

    Fix : 메인 페이지 오류 수정

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -1,63 +1,12 @@
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import Header from '../components/ui/Header';
+import alone_child from '../resources/images/alone_child.jpg';
 
-const Base = styled.div`
-  width: 100%;
-  height: 100vh;
-`;
+const Wrapper = styled.div``;
 
-const SliderObject = styled.div`
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-`;
-
-const TomatoBox = styled(SliderObject)`
-  background-color: lightcoral;
-`;
-
-const BlueBox = styled(SliderObject)`
-  background-color: skyblue;
-`;
-
-const GreenBox = styled(SliderObject)`
-  background-color: lightgreen;
-`;
-
-const GreyBox = styled(SliderObject)`
-  background-color: grey;
-`;
-
-const OrangeBox = styled(SliderObject)`
-  background-color: orange;
-`;
-
-interface IFullPage {
-  windowHeight: number;
-  currentPage: number;
-}
+const MainImg = styled.div``;
 
 function MainPage() {
-  const [pageState, setPageState] = useState<IFullPage>({
-    currentPage: 1,
-    windowHeight: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const { scrollTop, clientHeight } = document.documentElement;
-      const currentPage = Math.ceil(scrollTop / clientHeight);
-      const windowHeight = window.innerHeight;
-      setPageState({ currentPage, windowHeight });
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <Wrapper>
       <Header />
