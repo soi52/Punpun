185df83201a722d9b48f1d454753b78c7b471dd8
limecats <limecats0331@gmail.com>
2023-03-21 21:31:56 +0900

    feat: add ReservationCustomRepository & implement

---
+++ back/src/main/java/edu/ssafy/punpun/repository/ReservationCustomRepository.java
@@ -0,0 +1,11 @@
+package edu.ssafy.punpun.repository;
+
+import edu.ssafy.punpun.entity.Child;
+import edu.ssafy.punpun.entity.Reservation;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+
+public interface ReservationCustomRepository{
+    Page<Reservation> findAllByDate(Child child, LocalDateTime localDateTime, int page);
+}
