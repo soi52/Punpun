c09d6d4ef4cfabcee02b304de44a206637836bdb
limecats <limecats0331@gmail.com>
2023-03-21 10:22:00 +0900

    feat: ReservationEventPublisher
    예약을 하면 kafka를 통해서 이벤트 발급해주는 Component

---
+++ back/src/main/java/edu/ssafy/punpun/kafka/ReservationEventPublisher.java
@@ -0,0 +1,34 @@
+package edu.ssafy.punpun.kafka;
+
+import edu.ssafy.punpun.dto.ReservationEvent;
+import edu.ssafy.punpun.entity.Reservation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ReservationEventPublisher {
+    private final KafkaTemplate<String, ReservationEvent> template;
+
+    void publish(Reservation reservation) {
+        ListenableFuture<SendResult<String, ReservationEvent>> future = template.send("alarm", ReservationEvent.entityToEvent(reservation));
+        future.addCallback(new ListenableFutureCallback<SendResult<String, ReservationEvent>>() {
+            @Override
+            public void onSuccess(SendResult<String, ReservationEvent> result) {
+                log.info("[{}] Alarm Event Sent", reservation.getId());
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.warn("[{}] Alarm Event Send Fail", reservation.getId());
+            }
+        });
+    }
+}
