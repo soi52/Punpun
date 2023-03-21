2e0eb60599d99dfd1536230e09a0a7ff6d13e76c
jungeun park <pyooooong@gmail.com>
2023-03-21 17:50:12 +0900

    Feat : 후원 컴포넌트 수정

--- front/src/components/supporter/SuPointAdd.tsx
+++ front/src/components/supporter/SuPointAdd.tsx
@@ -7,6 +7,32 @@ const ComponentStyle = styled.div`
   padding: 20px;
 `;
 
+const PointButton = styled.button`
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  font-weight: bold;
+  color: black;
+  border-radius: 5px;
+  border: none;
+  background-color: #ffffff;
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  width: 90px;
+  height: 40px;
+  font-size: 16px;
+  font-weight: bold;
+  color: black;
+  border-radius: 25px;
+  border: none;
+  background-color: #e7e6f2;
+  cursor: pointer;
+`;
+
+const ButtonDiv = styled.div``;
+
 const SuPointAdd = () => {
   const mainMessage = {
     title: '',
@@ -24,7 +50,15 @@ const SuPointAdd = () => {
         message={`${mainMessage.ownerName}, ${mainMessage.message}`}
       />
       <h2>충전 포인트</h2>
+      <ButtonDiv>
+        <PointButton>10,000 P</PointButton>
+        <PointButton>30,000 P</PointButton>
+        <PointButton>50,000 P</PointButton>
+        <PointButton>70,000 P</PointButton>
+        <PointButton>100,000 P</PointButton>
+      </ButtonDiv>
       <h2>결제 수단</h2>
+      <Button>충전하기</Button>
     </ComponentStyle>
   );
 };
