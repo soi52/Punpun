4e8a3db6016f385b66d22582e0808e9ee52d290b
limecats <limecats0331@gmail.com>
2023-03-21 21:34:34 +0900

    feat: add dto and add mapping on controller

--- back/src/main/java/edu/ssafy/punpun/controller/BookingController.java
+++ back/src/main/java/edu/ssafy/punpun/controller/BookingController.java
@@ -1,13 +1,20 @@
 package edu.ssafy.punpun.controller;
 
 import edu.ssafy.punpun.dto.request.BookingRequestDTO;
+import edu.ssafy.punpun.dto.response.BookingChildResponseDTO;
 import edu.ssafy.punpun.entity.Child;
+import edu.ssafy.punpun.entity.Reservation;
 import edu.ssafy.punpun.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.AbstractListenerReadPublisher;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -20,5 +27,14 @@ public class BookingController {
     @ResponseStatus(HttpStatus.CREATED)
     public void booking(@AuthenticationPrincipal Child child, @RequestBody BookingRequestDTO requestDTO) {
         bookingService.reservation(child, requestDTO.getMenuId(), requestDTO.getReservationTime());
+    }
+
+    @GetMapping("/child")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<BookingChildResponseDTO> getBookings(@AuthenticationPrincipal Child child,
+                                                     @RequestParam(required = false, defaultValue = "0") int page,
+                                                     @RequestParam(required = false) LocalDateTime localDateTime) {
+        return bookingService.findReservations(child, localDateTime, page)
+                .map(BookingChildResponseDTO::entityToDto);
     }
 }
