6632cf2a3f393f8fe8e5f84a0b2a5f891b4923c1
limecats <limecats0331@gmail.com>
2023-03-22 13:25:04 +0900

    feat: add findByStore on service & test

--- back/src/main/java/edu/ssafy/punpun/service/BookingService.java
+++ back/src/main/java/edu/ssafy/punpun/service/BookingService.java
@@ -1,6 +1,9 @@
 package edu.ssafy.punpun.service;
 
+import edu.ssafy.punpun.dto.BookingSearchParamDTO;
+import edu.ssafy.punpun.dto.request.BookingRequestDTO;
 import edu.ssafy.punpun.entity.Child;
+import edu.ssafy.punpun.entity.Member;
 import edu.ssafy.punpun.entity.Reservation;
 import org.springframework.data.domain.Page;
 
@@ -10,4 +13,5 @@ public interface BookingService {
     Reservation reservation(Child child, Long menuId, LocalDateTime reservationTime);
 
     Page<Reservation> findReservations(Child child, LocalDateTime localDateTime, int page);
+    Page<Reservation> findAllByStore(Member owner, BookingSearchParamDTO params);
 }
--- back/src/main/java/edu/ssafy/punpun/service/BookingServiceImpl.java
+++ back/src/main/java/edu/ssafy/punpun/service/BookingServiceImpl.java
@@ -1,10 +1,12 @@
 package edu.ssafy.punpun.service;
 
+import edu.ssafy.punpun.dto.BookingSearchParamDTO;
 import edu.ssafy.punpun.entity.*;
 import edu.ssafy.punpun.entity.enumurate.ReservationState;
 import edu.ssafy.punpun.entity.enumurate.SupportReservationState;
 import edu.ssafy.punpun.entity.enumurate.SupportState;
 import edu.ssafy.punpun.exception.AlreadyEndException;
+import edu.ssafy.punpun.exception.NotStoreOwnerException;
 import edu.ssafy.punpun.kafka.ReservationEventPublisher;
 import edu.ssafy.punpun.repository.MenuRepository;
 import edu.ssafy.punpun.repository.ReservationRepository;
@@ -14,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import javax.sql.rowset.RowSetWarning;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 
@@ -58,5 +61,14 @@ public class BookingServiceImpl implements BookingService {
     @Override
     public Page<Reservation> findReservations(Child child, LocalDateTime localDateTime, int page) {
         return reservationRepository.findAllByDate(child, localDateTime, page);
+    }
+
+    @Override
+    public Page<Reservation> findAllByStore(Member owner, BookingSearchParamDTO params) {
+        owner.getStores().stream()
+                .filter(store -> store.getId().equals(params.getStoreId()))
+                .findFirst()
+                .orElseThrow(()-> new NotStoreOwnerException("가게의 주인이 아닙니다."));
+        return reservationRepository.findAllByStore(params);
     }
 }
