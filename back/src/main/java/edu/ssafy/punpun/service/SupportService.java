4ab5597b8edd31223267b1fab7dcd6c6472837d9
parksohui <psh990525@naver.com>
2023-03-22 17:51:19 +0900

    feat: add findShareList on Service

--- back/src/main/java/edu/ssafy/punpun/service/SupportService.java
+++ back/src/main/java/edu/ssafy/punpun/service/SupportService.java
@@ -1,11 +1,16 @@
 package edu.ssafy.punpun.service;
 
+import edu.ssafy.punpun.dto.response.ShareResponseDTO;
 import edu.ssafy.punpun.entity.Member;
 import edu.ssafy.punpun.entity.Support;
+import edu.ssafy.punpun.entity.enumurate.SupportType;
 
+import java.awt.print.Pageable;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SupportService {
     List<Support> findSupport(Member supporter);
     void saveSupport(List<Support> supportList, List<Long> menuId, List<Long>menuCount, Member supporter, Long usePoint);
+    List<ShareResponseDTO> findShareList(Long storeId, SupportType supportType, Pageable page, LocalDate date);
 }
--- back/src/main/java/edu/ssafy/punpun/service/SupportServiceImpl.java
+++ back/src/main/java/edu/ssafy/punpun/service/SupportServiceImpl.java
@@ -1,13 +1,18 @@
 package edu.ssafy.punpun.service;
 
+import edu.ssafy.punpun.dto.response.ShareResponseDTO;
 import edu.ssafy.punpun.entity.Member;
 import edu.ssafy.punpun.entity.Support;
+import edu.ssafy.punpun.entity.enumurate.SupportType;
 import edu.ssafy.punpun.repository.SupportRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.awt.print.Pageable;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Slf4j
@@ -28,6 +33,7 @@ public class SupportServiceImpl implements SupportService{
     public void saveSupport(List<Support> supportList, List<Long> menuId , List<Long> menuCount, Member supporter, Long usePoint) {
         // supporter use point
         // TODO: 영속성 확인
+        // TODO: 포인트 확인
         supporter.support(usePoint);
 
         for(int i=0; i<supportList.size(); i++){
@@ -38,5 +44,11 @@ public class SupportServiceImpl implements SupportService{
                 supportRepository.save(supportList.get(i));
             }
         }
+    }
+
+    @Override
+    public List<ShareResponseDTO> findShareList(Long storeId, SupportType supportType, Pageable page, LocalDate date) {
+//        return supportRepository.findShareList(storeId, supportType, date.atStartOfDay(), date.atTime(LocalTime.MAX) , page );
+        return null;
     }
 }
