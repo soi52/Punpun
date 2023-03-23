2aef846e1c0c0cca5d95c0754e78b8c2e576bb4e
parksohui <psh990525@naver.com>
2023-03-24 00:39:33 +0900

    feat: add getPoints on service

---
+++ back/src/main/java/edu/ssafy/punpun/service/PaymentService.java
@@ -0,0 +1,7 @@
+package edu.ssafy.punpun.service;
+
+import edu.ssafy.punpun.entity.Member;
+
+public interface PaymentService {
+    Long getPoints(Member member);
+}
---
+++ back/src/main/java/edu/ssafy/punpun/service/PaymentServiceImpl.java
@@ -0,0 +1,11 @@
+package edu.ssafy.punpun.service;
+
+import edu.ssafy.punpun.entity.Member;
+
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public Long getPoints(Member member) {
+        // member repository 호출
+        return null;
+    }
+}
