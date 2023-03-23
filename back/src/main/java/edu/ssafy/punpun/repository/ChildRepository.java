9f141b10dd9c36c5193be58fd061f7b1086cb658
박소연 <soyeon_9605@yu.ac.kr>
2023-03-23 17:14:13 +0900

    feat: child Repository 추가
    - findByEmail 추가

--- back/src/main/java/edu/ssafy/punpun/repository/ChildRepository.java
+++ back/src/main/java/edu/ssafy/punpun/repository/ChildRepository.java
@@ -4,6 +4,9 @@ import edu.ssafy.punpun.entity.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChildRepository extends JpaRepository<Child, Long> {
+    Optional<Child> findByEmail(String email);
 }
