bda16d80722492771f24812aa73001e5e15e9ff1
parksohui <psh990525@naver.com>
2023-03-23 16:04:05 +0900

    feat: add ShareResponseDTO

--- back/src/main/java/edu/ssafy/punpun/dto/response/ShareResponseDTO.java
+++ back/src/main/java/edu/ssafy/punpun/dto/response/ShareResponseDTO.java
@@ -1,34 +1,32 @@
 package edu.ssafy.punpun.dto.response;
 
-import edu.ssafy.punpun.entity.Support;
-import edu.ssafy.punpun.entity.enumurate.SupportState;
 import edu.ssafy.punpun.entity.enumurate.SupportType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class ShareResponseDTO {
     private Long supportId;
     private SupportType supportType;
-    private SupportState supportState;
     private LocalDate supportDate;
     private Long menuId;
     private String menuName;
     private Long totalCount;
     private Long useCount;
 
-    public ShareResponseDTO(Support support, Long totalCount, Long useCount){
-        this.supportId=support.getId();
-        this.supportType=support.getSupportType();
-        this.supportState=support.getSupportState();
-        this.supportDate=support.getCreatedDateTime().toLocalDate();
-        this.menuId=support.getMenu().getId();
-        this.menuName=support.getMenu().getName();
+
+    public ShareResponseDTO(SupportType supportType, LocalDateTime date, Long id, String name, Long totalCount){
+        this.supportType=supportType;
+        this.supportDate=date.toLocalDate();
+        this.menuId=id;
+        this.menuName=name;
         this.totalCount=totalCount;
-        this.useCount=useCount;
     }
 
 }
