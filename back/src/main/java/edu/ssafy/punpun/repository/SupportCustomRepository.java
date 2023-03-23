01fef6159ef139b91c0ddae187f8fb0e651f1eb8
parksohui <psh990525@naver.com>
2023-03-23 16:10:10 +0900

    feat: add findShareList on repository

--- back/src/main/java/edu/ssafy/punpun/repository/SupportCustomRepository.java
+++ back/src/main/java/edu/ssafy/punpun/repository/SupportCustomRepository.java
@@ -1,4 +1,11 @@
 package edu.ssafy.punpun.repository;
 
+import edu.ssafy.punpun.dto.response.ShareResponseDTO;
+import edu.ssafy.punpun.entity.enumurate.SupportType;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
+
 public interface SupportCustomRepository {
+    Page<ShareResponseDTO> findShareList (Long storeId, SupportType supportType, int page, LocalDate date);
 }
