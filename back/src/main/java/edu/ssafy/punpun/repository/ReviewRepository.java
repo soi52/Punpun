eb6514e3cb3b37622d72c21e979de4c61f3a7cac
limecats <limecats0331@gmail.com>
2023-03-23 12:52:36 +0900

    feat: add findAllBySupporter & test

--- back/src/main/java/edu/ssafy/punpun/repository/ReviewRepository.java
+++ back/src/main/java/edu/ssafy/punpun/repository/ReviewRepository.java
@@ -1,13 +1,21 @@
 package edu.ssafy.punpun.repository;
 
 import edu.ssafy.punpun.entity.Child;
+import edu.ssafy.punpun.entity.Member;
 import edu.ssafy.punpun.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findAllByChild(Child child, Pageable pageable);
+
+    @Query(value = "select r from Review r " +
+            "inner join r.reservation res " +
+            "inner join res.supportReservation sr " +
+            "inner join sr.support sup where sup.supporter = ?1")
+    Page<Review> findAllBySupporter(Member supporter, Pageable pageable);
 }
