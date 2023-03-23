2e58a88b63e611272564697a1524bced09b52a17
limecats <limecats0331@gmail.com>
2023-03-23 09:39:01 +0900

    feat: add find all review by child api & responseDto

---
+++ back/src/main/java/edu/ssafy/punpun/dto/response/ReviewChildResponseDTO.java
@@ -0,0 +1,28 @@
+package edu.ssafy.punpun.dto.response;
+
+import edu.ssafy.punpun.entity.Keyword;
+import edu.ssafy.punpun.entity.Review;
+import edu.ssafy.punpun.entity.ReviewKeyword;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
+public class ReviewChildResponseDTO {
+    private Long reviewId;
+    private String reviewContent;
+    private List<Keyword> keywords;
+
+    public static ReviewChildResponseDTO entityToDto(Review review) {
+        Long reviewId = review.getId();
+        String reviewContent = review.getContent();
+        List<Keyword> keywords = review.getReviewKeywords().stream()
+                .map(ReviewKeyword::getKeyword)
+                .collect(Collectors.toList());
+
+        return new ReviewChildResponseDTO(reviewId, reviewContent, keywords);
+    }
+}
