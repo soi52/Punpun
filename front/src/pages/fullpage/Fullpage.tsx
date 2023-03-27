dd0b03d65ca5daaad58380fe454e8697afb13dc1
이햇살 <leeee99@hanmail.net>
2023-03-28 00:00:03 +0900

    Feat : Fullpage 구현

---
+++ front/src/pages/fullpage/Fullpage.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import ReactPageScroller, { SectionContainer } from 'react-page-scroller';
+import { Pager } from 'react-bootstrap';
+import './Fullpage.css';
+
+import FirstComponent from './FirstComponent';
+import SecondComponent from './SecondComponent';
+import ThirdComponent from './ThirdComponent';
+import FourthComponent from './FourthComponent';
+import FifthComponent from './FifthComponent';
+
+type FullPageState = {
+  currentPage: number | null;
+};
+
+const FullPage = () => {
+  const [currentPage, setCurrentPage] = useState<number | null>(null);
+
+  const handlePageChange: (eventKey: any) => void = (number) => {
+    setCurrentPage(number);
+  };
+
+  const handleBeforePageChange = (number: number) => {
+    console.log(number);
+  };
+
+  const getPagesNumbers = () => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= 5; i++) {
+      pageNumbers.push(
+        <Pager.Item key={i} eventKey={i - 1} onSelect={handlePageChange}>
+          {i}
+        </Pager.Item>
+      );
+    }
+
+    return [...pageNumbers];
+  };
+
+  const pagesNumbers = getPagesNumbers();
+
+  return (
+    <>
+      <ReactPageScroller
+        pageOnChange={handlePageChange}
+        onBeforePageScroll={handleBeforePageChange}
+        customPageNumber={currentPage ?? undefined}
+      >
+        <FirstComponent />
+        <SectionContainer height={50}>
+          <SecondComponent />
+        </SectionContainer>
+        <ThirdComponent />
+        <FourthComponent />
+        <FifthComponent />
+      </ReactPageScroller>
+      {/* <Pager className="pagination-additional-class" bsSize="large">
+        {pagesNumbers}
+      </Pager> */}
+    </>
+  );
+};
+
+export default FullPage;
