8ca4f68b3d77158ab38736124b60edacd893f2f5
limecats <limecats0331@gmail.com>
2023-03-27 21:18:59 +0900

    fix: mock service for test only controller
    컨트롤러를 테스트함이 목적임으로 service를 모킹하는 것으로 변경함

--- back/src/test/java/edu/ssafy/punpun/controller/BookingControllerTest.java
+++ back/src/test/java/edu/ssafy/punpun/controller/BookingControllerTest.java
@@ -2,30 +2,24 @@ package edu.ssafy.punpun.controller;
 
 import com.google.gson.Gson;
 import edu.ssafy.punpun.dto.response.ErrorDTO;
+import edu.ssafy.punpun.entity.Reservation;
+import edu.ssafy.punpun.entity.enumurate.ReservationState;
 import edu.ssafy.punpun.exception.AlreadyEndException;
-import edu.ssafy.punpun.kafka.ReservationEventPublisher;
-import edu.ssafy.punpun.service.BookingServiceImpl;
+import edu.ssafy.punpun.service.BookingService;
 import edu.ssafy.testutil.WIthCustomChild;
 import edu.ssafy.punpun.dto.request.BookingRequestDTO;
 import edu.ssafy.punpun.entity.Child;
-import edu.ssafy.punpun.entity.Menu;
-import edu.ssafy.punpun.entity.Support;
-import edu.ssafy.punpun.entity.enumurate.SupportState;
 import edu.ssafy.punpun.entity.enumurate.UserRole;
-import edu.ssafy.punpun.repository.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -39,22 +33,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BookingControllerTest {
     @Autowired
     private MockMvc mockMvc;
-    @SpyBean
-    private BookingServiceImpl bookingService;
     @MockBean
-    private MenuRepository menuRepository;
-    @MockBean
-    private SupportRepository supportRepository;
-    @MockBean
-    private ReservationRepository reservationRepository;
-    @MockBean
-    private SupportReservationRepository supportReservationRepository;
-    @MockBean
-    private ReservationEventPublisher publisher;
+    private BookingService bookingService;
 
     @Nested
     @DisplayName("예약 등록하기")
-    public class Reservation {
+    public class ReservationApi {
         @Test
         @WIthCustomChild
         @DisplayName("성공")
@@ -66,22 +50,12 @@ class BookingControllerTest {
                     .email("email@email.com")
                     .role(UserRole.CHILD)
                     .build();
-            Menu menu = Menu.builder()
+            Reservation reservation = Reservation.builder()
                     .id(1L)
-                    .build();
-            Support support = Support.builder()
-                    .menu(menu)
-                    .supportState(SupportState.SUPPORT)
+                    .state(ReservationState.BOOKING)
                     .build();
 
-            //아무것도 안하는 애들
-            doReturn(null).when(reservationRepository).save(any());
-            doReturn(null).when(supportReservationRepository).save(any());
-            doNothing().when(publisher).publish(any());
-
-            doReturn(Optional.of(menu)).when(menuRepository).findById(1L);
-            doReturn(List.of(support)).when(supportRepository).findAllByMenu(menu);
-            bookingService = new BookingServiceImpl(supportReservationRepository, reservationRepository, supportRepository, menuRepository, publisher);
+            doReturn(reservation).when(bookingService).reservation(child, 1L, now);
 
             BookingRequestDTO bookingRequestDTO = new BookingRequestDTO();
             bookingRequestDTO.setMenuId(1L);
@@ -94,10 +68,6 @@ class BookingControllerTest {
                             .content(input))
                     .andExpect(status().isCreated())
                     .andDo(print());
-
-            verify(reservationRepository, times(1)).save(any());
-            verify(supportReservationRepository, times(1)).save(any());
-            verify(publisher, times(1)).publish(any());
         }
 
         @Test
@@ -105,14 +75,14 @@ class BookingControllerTest {
         @DisplayName("실패 - 메뉴 없음")
         void reservationFailNoMenu() throws Exception {
             LocalDateTime now = LocalDateTime.now();
-
-            doReturn(null).when(supportRepository).findAllByMenu(any());
-            doReturn(null).when(reservationRepository).save(any());
-            doReturn(null).when(supportReservationRepository).save(any());
-            doNothing().when(publisher).publish(any());
-
-            doReturn(Optional.empty()).when(menuRepository).findById(1L);
-            bookingService = new BookingServiceImpl(supportReservationRepository, reservationRepository, supportRepository, menuRepository, publisher);
+            Child child = Child.builder()
+                    .id(1L)
+                    .name("name")
+                    .email("email@email.com")
+                    .role(UserRole.CHILD)
+                    .build();
+            when(bookingService.reservation(any(Child.class), eq(1L), eq(now)))
+                    .thenThrow(new IllegalArgumentException("없는 메뉴입니다."));
 
             BookingRequestDTO bookingRequestDTO = new BookingRequestDTO();
             bookingRequestDTO.setMenuId(1L);
@@ -130,11 +100,6 @@ class BookingControllerTest {
                     .andExpect(content().contentType("application/json;charset=UTF-8"))
                     .andExpect(content().string(output))
                     .andDo(print());
-
-            verify(supportRepository, times(0)).save(any());
-            verify(reservationRepository, times(0)).save(any());
-            verify(supportReservationRepository, times(0)).save(any());
-            verify(publisher, times(0)).publish(any());
         }
 
         @Test
@@ -142,21 +107,14 @@ class BookingControllerTest {
         @DisplayName("실패 - 모두 예약됨")
         void reservationFailAllReservation() throws Exception {
             LocalDateTime now = LocalDateTime.now();
-            Menu menu = Menu.builder()
+            Child child = Child.builder()
                     .id(1L)
+                    .name("name")
+                    .email("email@email.com")
+                    .role(UserRole.CHILD)
                     .build();
-            Support support = Support.builder()
-                    .menu(menu)
-                    .supportState(SupportState.BOOKING)
-                    .build();
-
-            doReturn(null).when(reservationRepository).save(any());
-            doReturn(null).when(supportReservationRepository).save(any());
-            doNothing().when(publisher).publish(any());
-
-            doReturn(Optional.of(menu)).when(menuRepository).findById(1L);
-            doReturn(List.of(support)).when(supportRepository).findAllByMenu(menu);
-            bookingService = new BookingServiceImpl(supportReservationRepository, reservationRepository, supportRepository, menuRepository, publisher);
+            when(bookingService.reservation(any(), eq(1L), eq(now)))
+                    .thenThrow(new AlreadyEndException("이미 모두 예약되었습니다."));
 
             BookingRequestDTO bookingRequestDTO = new BookingRequestDTO();
             bookingRequestDTO.setMenuId(1L);
@@ -174,10 +132,6 @@ class BookingControllerTest {
                     .andExpect(content().contentType("application/json;charset=UTF-8"))
                     .andExpect(content().string(output))
                     .andDo(print());
-
-            verify(reservationRepository, times(0)).save(any());
-            verify(supportReservationRepository, times(0)).save(any());
-            verify(publisher, times(0)).publish(any());
         }
     }
 }
