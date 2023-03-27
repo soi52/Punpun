08860e61e63505d27652762ac5f438ad9e83582c
Jungeun Park <pyooooong@gmail.com>
2023-03-27 20:13:02 +0900

    Modify : 배포 서버 확인

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
+import axios from 'axios';
 import './index.css';
+
+axios.defaults.baseURL = "https://j8d109.p.ssafy.io/";
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
