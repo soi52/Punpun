29ccef537a4bb3085626a8a48b68652973c336e6
jungeun park <pyooooong@gmail.com>
2023-03-27 10:37:58 +0900

    Feat : 카카오로그인 AccessToken 발급

--- front/src/pages/RedirectPage.tsx
+++ front/src/pages/RedirectPage.tsx
@@ -1,10 +1,29 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { setCookie } from '../components/auth/Cookie';
 
 function RedirectPage() {
-  useEffect(() => {
-    // const token = getUriParameter('accessToken');
+  const navigate = useNavigate();
+  const getUrlParameter = (name:string) => {
+    // 쿼리 파라미터에서 값을 추출해주는 함수
+    let search = window.location.search;
+    let params = new URLSearchParams(search);
+    return params.get(name);
+  };
 
-  })
+  useEffect(() => {
+    const token = getUrlParameter('accessToken');
+    console.log(token);
+    
+    const accessToken = token || '';
+    setCookie('accessToken', accessToken, {
+      path: '/',
+      secure: true,
+      // httpOnly: true,
+      sameSite: 'none',
+    });
+  }, []);
+  
   return (
     <div>
       <h1>메인 페이지</h1>
