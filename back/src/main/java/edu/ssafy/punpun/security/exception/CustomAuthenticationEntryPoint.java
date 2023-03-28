76462b5996f3a231093b9369aeadd28409332a33
limecats <limecats0331@gmail.com>
2023-03-28 09:08:45 +0900

    fix: resolve conflict

--- back/src/main/java/edu/ssafy/punpun/security/exception/CustomAuthenticationEntryPoint.java
+++ back/src/main/java/edu/ssafy/punpun/security/exception/CustomAuthenticationEntryPoint.java
@@ -21,11 +21,10 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         ObjectMapper objectMapper = new ObjectMapper();
         log.info("[AuthenticationEntryPoint - cmmence] 인증 실패로 response.sendError 발생");
 
-        ErrorDTO errorDTO = new ErrorDTO("AuthenticationEntryPoint_commence", "인증이 실패하였습니다.");
-
         response.setStatus(401);
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write(objectMapper.writeValueAsString(errorDTO));
+        response.getWriter().write("AuthenticationEntryPoint_commence 인증이 실패하였습니다. - ErrorCustomAuthenticationEntryPoint");
+//        response.getWriter().write(objectMapper.writeValueAsString(errorDTO));
     }
 }
