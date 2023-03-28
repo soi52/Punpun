79ef628630f0d862a52d3dde5cbaaf4cbf6bd057
parksohui <psh990525@naver.com>
2023-03-29 00:46:59 +0900

    modify : modify LocalDateTime to String

--- back/src/main/java/edu/ssafy/punpun/dto/response/ShareResponseDTO.java
+++ back/src/main/java/edu/ssafy/punpun/dto/response/ShareResponseDTO.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -14,16 +13,15 @@ import java.time.LocalDateTime;
 public class ShareResponseDTO {
     private Long supportId;
     private SupportType supportType;
-    private LocalDate supportDate;
+    private String supportDate;
     private Long menuId;
     private String menuName;
     private Long totalCount;
     private Long useCount;
 
-
     public ShareResponseDTO(SupportType supportType, LocalDateTime date, Long id, String name, Long totalCount){
         this.supportType=supportType;
-        this.supportDate=date.toLocalDate();
+        this.supportDate=date.toLocalDate().toString();
         this.menuId=id;
         this.menuName=name;
         this.totalCount=totalCount;
