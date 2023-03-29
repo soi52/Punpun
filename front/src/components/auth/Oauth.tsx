9b7b6af93f7c69113150ac3e63ef4b56e65fb74d
haetsal <leeee99@hanmail.net>
2023-03-29 22:04:18 +0900

    Modify : 컴포넌트 명 수정

--- front/src/components/auth/Oauth.tsx
+++ front/src/components/auth/Oauth.tsx
@@ -46,29 +46,29 @@ const Oauth = () => {
     return params.get(name);
   };
 
-  const getLocation = () => {
-    const location = UseGeolocation();
-    const { latitude = 0, longitude = 0 } =
-      typeof location === 'object' ? location : {};
+  // const getLocation = () => {
+  //   const location = UseGeolocation();
+  //   const { latitude = 0, longitude = 0 } =
+  //     typeof location === 'object' ? location : {};
 
-    let geocoder = new kakao.maps.services.Geocoder();
-    let coord = new kakao.maps.LatLng(latitude, longitude);
+  //   let geocoder = new kakao.maps.services.Geocoder();
+  //   let coord = new kakao.maps.LatLng(latitude, longitude);
 
-    let callback = function (result: any, status: any) {
-      if (status === kakao.maps.services.Status.OK) {
-        setAddress(
-          result[0].address.region_1depth_name +
-            ' ' +
-            result[0].address.region_2depth_name
-        );
-      }
-    };
+  //   let callback = function (result: any, status: any) {
+  //     if (status === kakao.maps.services.Status.OK) {
+  //       setAddress(
+  //         result[0].address.region_1depth_name +
+  //           ' ' +
+  //           result[0].address.region_2depth_name
+  //       );
+  //     }
+  //   };
 
-    geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-  };
-  useEffect(() => {
-    getLocation();
-  });
+  //   geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+  // };
+  // useEffect(() => {
+  //   getLocation();
+  // });
 
   useEffect(() => {
     const token = getUrlParameter('token');
