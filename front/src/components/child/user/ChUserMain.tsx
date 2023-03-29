f89d0da8383c41c1bd8015310dff750e5c1c0bff
jungeun park <pyooooong@gmail.com>
2023-03-29 13:26:22 +0900

    Feat : 프로필에 실제 데이터 렌더링

--- front/src/components/child/user/ChUserMain.tsx
+++ front/src/components/child/user/ChUserMain.tsx
@@ -5,6 +5,7 @@ import MainMessage from '../../ui/MainMessage';
 
 import TodayBooking from './TodayBooking';
 import Message from './Message';
+import ChMainMessage from '../ChMainMessage';
 
 const ComponentStyle = styled.div`
   padding: 20px;
@@ -19,21 +20,10 @@ const MessageDiv = styled.div`
 `;
 
 const ChUserMain = () => {
-  const mainMessage = {
-    title: '',
-    ownerName: '정은 학생',
-    message: '오늘도 맛있는 밥 먹어요~!',
-    name: '박정은 학생',
-  };
 
   return (
     <ComponentStyle>
-      <h2>
-        <MainTitle title={`${mainMessage.name} ${mainMessage.title}`} />
-      </h2>
-      <MainMessage
-        message={`${mainMessage.ownerName}, ${mainMessage.message}`}
-      />
+      <ChMainMessage/>
       <BookingDiv>
         <h2>오늘의 예약</h2>
         <TodayBooking />
