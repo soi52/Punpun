aba18e6a684e2b7d0fac8153add1cea656847121
박소연 <soyeon_9605@yu.ac.kr>
2023-03-29 11:12:41 +0900

    modify : Test UserController
    - updateMemberInfo 변경

--- back/src/test/java/edu/ssafy/punpun/controller/UserControllerTest.java
+++ back/src/test/java/edu/ssafy/punpun/controller/UserControllerTest.java
@@ -13,12 +13,14 @@ import edu.ssafy.testutil.WIthCustomChild;
 import edu.ssafy.testutil.WIthCustomSupporter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -89,6 +91,8 @@ public class UserControllerTest {
 
         MemberRequestDTO memberRequestDTO = new MemberRequestDTO("01000000000");
         String input = new Gson().toJson(memberRequestDTO);
+
+        doNothing().when(userService).updateMemberInfo(member.getId(), "01000000000");
 
         mockMvc.perform(patch("/users/member/phone")
                         .with(csrf())
