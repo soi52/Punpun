85366f99e259d7c46a0aec85403c3fae27843445
limecats <limecats0331@gmail.com>
2023-03-30 17:29:38 +0900

    fix: hotfix auditing

---
+++ back/src/main/java/edu/ssafy/punpun/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package edu.ssafy.punpun.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
