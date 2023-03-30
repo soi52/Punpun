aa2860246137972c3ebcd5f44917088fca263d62
박소연 <soyeon_9605@yu.ac.kr>
2023-03-31 02:29:16 +0900

    feat : StoreAPI getStoreDetailChild
    - getStoreDetailChild
    - Controller, Service
    - java, test

--- back/src/main/java/edu/ssafy/punpun/service/StoreService.java
+++ back/src/main/java/edu/ssafy/punpun/service/StoreService.java
@@ -1,5 +1,7 @@
 package edu.ssafy.punpun.service;
 
+import edu.ssafy.punpun.dto.response.FavoriteMenuDTO;
+import edu.ssafy.punpun.entity.Child;
 import edu.ssafy.punpun.entity.Member;
 import edu.ssafy.punpun.entity.Store;
 
@@ -7,6 +9,7 @@ import java.util.List;
 
 public interface StoreService {
     Store findById(Long id);
+    List<FavoriteMenuDTO> getStoreDetailChild(Store store, Child child);
     List<Store> findByNameContaining(String name);
     List<Store> findByOwner(Member member);
     void deleteStoreByMember(Member member, Long storeId);
--- back/src/main/java/edu/ssafy/punpun/service/StoreServiceImpl.java
+++ back/src/main/java/edu/ssafy/punpun/service/StoreServiceImpl.java
@@ -1,14 +1,21 @@
 package edu.ssafy.punpun.service;
 
+import edu.ssafy.punpun.dto.response.FavoriteMenuDTO;
+import edu.ssafy.punpun.entity.Child;
+import edu.ssafy.punpun.entity.FavoriteMenu;
 import edu.ssafy.punpun.entity.Member;
 import edu.ssafy.punpun.entity.Store;
 import edu.ssafy.punpun.exception.NotStoreOwnerException;
+import edu.ssafy.punpun.repository.FavoriteMenuRepository;
+import edu.ssafy.punpun.repository.MenuRepository;
 import edu.ssafy.punpun.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -16,11 +23,28 @@ import java.util.List;
 public class StoreServiceImpl implements StoreService {
 
     private final StoreRepository storeRepository;
+    private final MenuRepository menuRepository;
+    private final FavoriteMenuRepository favoriteMenuRepository;
 
     @Override
     public Store findById(Long id) {
         return storeRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 가게 입니다."));
+    }
+
+    public List<FavoriteMenuDTO> getStoreDetailChild(Store store, Child child) {
+        List<FavoriteMenuDTO> favoriteMenuDTOList =
+                menuRepository.findByStore(store).stream()
+                        .map(menu -> {
+                            Optional<FavoriteMenu> favoriteMenu = favoriteMenuRepository.findByChildAndMenu(child, menu);
+                            if (favoriteMenu != null) {
+                                return new FavoriteMenuDTO(menu, true);
+                            } else {
+                                return new FavoriteMenuDTO(menu, false);
+                            }
+                        })
+                        .collect(Collectors.toList());
+        return favoriteMenuDTOList;
     }
 
     @Override
