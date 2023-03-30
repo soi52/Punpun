881a1f16b12302010e5f384721bd247f5bfd68fe
haetsal <leeee99@hanmail.net>
2023-03-30 11:58:00 +0900

    Modify : 유저 프로필 코드 수정

--- front/src/components/ui/Profile.tsx
+++ front/src/components/ui/Profile.tsx
@@ -45,37 +45,37 @@ function Profile() {
   const [userInfo, setUserInfo] = useRecoilState(userInfoState);
   console.log(userInfo);
 
-  const location = useGeolocation();
-  const { latitude = 0, longitude = 0 } =
-    typeof location === 'object' ? location : {};
+  // const location = useGeolocation();
+  // const { latitude = 0, longitude = 0 } =
+  //   typeof location === 'object' ? location : {};
 
-  function getAddr(lat: any, lng: any) {
-    let geocoder = new kakao.maps.services.Geocoder();
+  // function getAddr(lat: any, lng: any) {
+  //   let geocoder = new kakao.maps.services.Geocoder();
 
-    let coord = new kakao.maps.LatLng(lat, lng);
-    let callback = function (result: any, status: any) {
-      if (status === kakao.maps.services.Status.OK) {
-        const arr = { ...result };
-        const _arr =
-          arr[0].address.region_1depth_name +
-          ' ' +
-          arr[0].address.region_2depth_name;
-        setUserInfo((userInfo) => {
-          return {
-            ...userInfo,
-            userLocation: _arr,
-          };
-        });
-      }
-    };
-    geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-  }
+  //   let coord = new kakao.maps.LatLng(lat, lng);
+  //   let callback = function (result: any, status: any) {
+  //     if (status === kakao.maps.services.Status.OK) {
+  //       const arr = { ...result };
+  //       const _arr =
+  //         arr[0].address.region_1depth_name +
+  //         ' ' +
+  //         arr[0].address.region_2depth_name;
+  //       setUserInfo((userInfo) => {
+  //         return {
+  //           ...userInfo,
+  //           userLocation: _arr,
+  //         };
+  //       });
+  //     }
+  //   };
+  //   geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+  // }
 
-  useEffect(() => {
-    if (typeof location === 'object') {
-      getAddr(latitude, longitude);
-    }
-  }, [location]);
+  // useEffect(() => {
+  //   if (typeof location === 'object') {
+  //     getAddr(latitude, longitude);
+  //   }
+  // }, [location]);
 
   return (
     <div>
