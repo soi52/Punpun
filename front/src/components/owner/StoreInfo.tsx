fc378daca2f09cbf62ebdd044fca9d15f3fa4e2d
haetsal <leeee99@hanmail.net>
2023-03-31 15:44:52 +0900

    Feat : 가게 메인페이지 문구 수정

--- front/src/components/owner/StoreInfo.tsx
+++ front/src/components/owner/StoreInfo.tsx
@@ -1,19 +1,34 @@
+import { useRecoilValue } from 'recoil';
+import {
+  owStoreState,
+  selectedStoreState,
+  userInfoState,
+} from '../../store/atoms';
 import MainMessage from '../ui/MainMessage';
 import MainTitle from '../ui/MainTitle';
 
 function StoreInfo() {
+  const userInfo = useRecoilValue(userInfoState);
+  const selectedStore = useRecoilValue(selectedStoreState);
+  const stores = useRecoilValue(owStoreState);
+
   const storeInfo = {
     title: '따뜻한 사랑과 건강한 식사를 나눠주시는',
     ownerName: '김싸피 사장님',
     message: '어느새 10번의 식사 나눔을 하셨네요!',
     name: '싸피식당',
   };
+
+  const storeName = selectedStore
+    ? selectedStore.storeName
+    : stores[0].storeName;
+
   return (
     <>
       <h2>
-        <MainTitle title={`${storeInfo.title} ${storeInfo.name}`} />
+        <MainTitle title={`${storeInfo.title} ${storeName}`} />
       </h2>
-      <MainMessage message={`${storeInfo.ownerName}, ${storeInfo.message}`} />
+      <MainMessage message={`${userInfo.userName}, ${storeInfo.message}`} />
     </>
   );
 }
