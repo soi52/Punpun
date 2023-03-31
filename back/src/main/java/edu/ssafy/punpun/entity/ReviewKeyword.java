e9cb513d7eb8d5759c50407843a2b57eea72d55c
limecats <limecats0331@gmail.com>
2023-03-31 15:48:31 +0900

    fix: hotfix - add JoinColumn

--- back/src/main/java/edu/ssafy/punpun/entity/ReviewKeyword.java
+++ back/src/main/java/edu/ssafy/punpun/entity/ReviewKeyword.java
@@ -18,7 +18,9 @@ public class ReviewKeyword extends BaseEntity {
     @Column(name = "review_keyword_id")
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
     private Review review;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyword_id")
     private Keyword keyword;
 }
