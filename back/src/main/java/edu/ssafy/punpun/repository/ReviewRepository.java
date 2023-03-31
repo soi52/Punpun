48a19790082a5b8e90d02b44b4c7202a12b8a474
limecats <limecats0331@gmail.com>
2023-03-31 14:10:37 +0900

    fix: jpql change to left  join fetch

--- back/src/main/java/edu/ssafy/punpun/repository/ReviewRepository.java
+++ back/src/main/java/edu/ssafy/punpun/repository/ReviewRepository.java
@@ -12,15 +12,23 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query(value = "select c from Child c inner join c.favoriteMenus fm inner join fm.menu where c = ?1"
-    ,countQuery = "select count(c) from Child c inner join c.favoriteMenus fm inner join fm.menu where c = ?1")
+    @Query(value = "select r from Review r left join fetch r.reviewKeywords rk left join fetch rk.review where r.child = ?1"
+            , countQuery = "select count(r) from Review r where r.child = ?1")
     Page<Review> findAllByChild(Child child, Pageable pageable);
 
     @Query(value = "select r from Review r " +
+            "left join fetch r.reviewKeywords rk " +
+            "left join fetch rk.keyword " +
+            "inner join r.reservation res " +
+            "inner join res.supportReservation sr " +
+            "inner join sr.support sup where sup.supporter = ?1",
+    countQuery = "select count(r) from Review r " +
             "inner join r.reservation res " +
             "inner join res.supportReservation sr " +
             "inner join sr.support sup where sup.supporter = ?1")
     Page<Review> findAllBySupporter(Member supporter, Pageable pageable);
 
+    @Query(value = "select r from Review r left join fetch r.reviewKeywords rk left join fetch rk.review where r.store = ?1"
+            , countQuery = "select count(r) from Review r where r.store = ?1")
     Page<Review> findAllByStore(Store store, Pageable pageable);
 }
