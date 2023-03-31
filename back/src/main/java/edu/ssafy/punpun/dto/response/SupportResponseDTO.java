839ff19ea677153b26fd1bdaa40710f1e110bb52
parksohui <psh990525@naver.com>
2023-03-31 16:38:04 +0900

    delete : delete LocalDateTime

--- back/src/main/java/edu/ssafy/punpun/dto/response/SupportResponseDTO.java
+++ back/src/main/java/edu/ssafy/punpun/dto/response/SupportResponseDTO.java
@@ -5,8 +5,6 @@ import edu.ssafy.punpun.entity.enumurate.SupportState;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @AllArgsConstructor
 public class SupportResponseDTO {
