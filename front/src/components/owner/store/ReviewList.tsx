30d7d32b46d1012f4bfef2cd9a03198655d583d1
haetsal <leeee99@hanmail.net>
2023-03-31 17:26:30 +0900

    Feat : 사장 스토어 리뷰 연결

--- front/src/components/owner/store/ReviewList.tsx
+++ front/src/components/owner/store/ReviewList.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import API from '../../../store/API';
-import { reviewState } from '../../../store/atoms';
+import { reviewState, selectedStoreState } from '../../../store/atoms';
 import ReviewItem from '../../ui/ReviewItem';
 import StoreInfo from '../StoreInfo';
 
@@ -10,18 +10,35 @@ const Wrapper = styled.div`
   padding: 20px;
 `;
 
+interface Reviews {
+  reviewId: number;
+  reviewContent: string;
+  keywords: { content: string }[]; // keywords 배열 요소 타입을 정의합니다.
+  childId: number;
+  childName: string;
+  childProfileUrl: string;
+}
+
 function ReviewList() {
-  const reviews = useRecoilValue(reviewState);
+  const [owReviews, setOwReviews] = useState<Reviews[]>([]); // 배열 상태를 명시합니다.
+  const selectedStore = useRecoilValue(selectedStoreState);
 
   useEffect(() => {
-    API.get(`reviews/stores`);
-  });
+    API.get(`reviews/stores/${selectedStore?.storeId}`)
+      .then((response: any) => {
+        console.log(response.data.content); // 받아온 데이터를 owReviews에 업데이트합니다.
+        setOwReviews(response.data.content); // 받아온 데이터를 owReviews에 업데이트합니다.
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
+  }, [selectedStore?.storeId]);
 
   return (
     <Wrapper>
       <StoreInfo />
       <h2>감사인사 목록</h2>
-      <ReviewItem reviews={reviews} />
+      <ReviewItem reviews={owReviews} />
     </Wrapper>
   );
 }
