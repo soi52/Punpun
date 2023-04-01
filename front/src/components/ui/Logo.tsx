7132508c4de8c6e3a0b952272a1a3e196147f10f
Jungeun Park <pyooooong@gmail.com>
2023-04-02 01:22:18 +0900

    Modify : 선호메뉴 컴포넌트 상태관리 수정

--- front/src/components/ui/Logo.tsx
+++ front/src/components/ui/Logo.tsx
@@ -1,28 +1,45 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import logoIMG from '../../resources/images/logo.png';
-// import logoIMG from '../../resources/images/thumb.png';
+import logoIMG1 from '../../resources/images/logo (1).png';
+import logoIMG2 from '../../resources/images/logo (2).png';
+import logoIMG3 from '../../resources/images/logo (3).png';
 
-// const LogoTitle = styled.h3`
-//   text-align: center;
-//   float: left;
-//   cursor: pointer;
-// `;
+const bounce = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-5px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+const LogoImgWrapper = styled.div`
+  display: flex;
+`;
 
 const LogoImg = styled.div`
-  background: url(${logoIMG}) no-repeat center;
-  width: 100px;
-  background-size: 100%;
+  display: flex;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  animation: ${bounce} 1s ease-in-out infinite;
 `;
 
 function Logo() {
   return (
     <>
-      <img
-        src={logoIMG}
-        alt="logo"
-        width="100px"
-        style={{ marginLeft: '10px' }}
-      />
+      <LogoImgWrapper>
+        <LogoImg style={{ backgroundImage: `url(${logoIMG1})`, animationDelay: '0s' }} />
+        <LogoImg style={{ backgroundImage: `url(${logoIMG1})`, animationDelay: '0.2s' }} />
+        <LogoImg style={{ backgroundImage: `url(${logoIMG2})`, animationDelay: '0.4s' }} />
+        <LogoImg style={{ backgroundImage: `url(${logoIMG3})`, animationDelay: '0.6s' }} />
+      </LogoImgWrapper>
     </>
   );
 }
