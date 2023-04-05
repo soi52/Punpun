d116ffc4e8d4a38e3bc630f8bda034691c9490e6
haetsal <leeee99@hanmail.net>
2023-04-05 17:52:14 +0900

    Feat : 나눔 횟수

--- front/src/components/owner/StoreInfo.tsx
+++ front/src/components/owner/StoreInfo.tsx
@@ -3,6 +3,7 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import {
   owStoreState,
   selectedStoreState,
+  ShareListState,
   userInfoState,
 } from '../../store/atoms';
 import MainMessage from '../ui/MainMessage';
@@ -12,14 +13,8 @@ import API from '../../store/API';
 function StoreInfo() {
   const userInfo = useRecoilValue(userInfoState);
   const [selectedStore, setSelectedStore] = useRecoilState(selectedStoreState);
+  const [shareList, setShareList] = useRecoilState(ShareListState);
   const stores = useRecoilValue(owStoreState);
-
-  const storeInfo = {
-    title: '따뜻한 사랑과 건강한 식사를 나눠주시는',
-    ownerName: '김싸피 사장님',
-    message: '어느새 10번의 식사 나눔을 하셨네요!',
-    name: '싸피식당',
-  };
 
   useEffect(() => {
     if (!selectedStore) {
@@ -34,15 +29,34 @@ function StoreInfo() {
           console.error(error);
         });
     }
-  }, [setSelectedStore, stores]);
+  }, [setSelectedStore, stores, shareList]);
+
+  useEffect(() => {
+    const config = {
+      params: {
+        page: 0,
+        type: 'SHARE',
+      },
+    };
+    API.get(`supports/${selectedStore?.storeId}`, config)
+      .then((response) => {
+        setShareList(response.data.content);
+        console.log(response.data.content);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
       <h2>
-        <MainTitle title={`${storeInfo.title} ${selectedStore?.storeName}`} />
+        <MainTitle
+          title={`따뜻한 사랑과 건강한 식사를 나눠주시는 ${selectedStore?.storeName}`}
+        />
       </h2>
       <MainMessage
-        message={`　${userInfo.userName} 사장님, ${storeInfo.message}`}
+        message={`　${userInfo.userName} 사장님, 어느새 ${shareList.length}번의 식사 나눔을 하셨네요!`}
       />
     </>
   );
