2d76fabf9401c63cbbd242df655f61a59e99bda6
Jungeun Park <pyooooong@gmail.com>
2023-04-05 23:30:15 +0900

    Design : 컴포넌트 규격 수정

--- front/src/components/supporter/SuDetail.tsx
+++ front/src/components/supporter/SuDetail.tsx
@@ -7,12 +7,18 @@ const ComponentStyle = styled.div`
   padding: 20px;
 `;
 
+const Div = styled.div`
+  padding-top: 20px;
+`;
+
 function SuDetail() {
   return (
     <ComponentStyle>
       <SuMainMessage />
-      <h2>후원내역</h2>
-      <SupportHistory />
+      <Div>
+        <h2>후원내역</h2>
+        <SupportHistory />
+      </Div>
     </ComponentStyle>
   );
 }
