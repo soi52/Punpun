e4a6b3916b71ab4e8e6bcd99b8995dc59446ac95
haetsal <leeee99@hanmail.net>
2023-04-05 14:03:25 +0900

    Design : 배경 색 변경

--- front/src/GlobalStyle.tsx
+++ front/src/GlobalStyle.tsx
@@ -8,7 +8,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: "Helvetica", "Arial", sans-serif;
     line-height: 1.5;
-    background-color: #f0f0f0;
+    background-color: #F1F3F4;
   }
 
   ::-webkit-scrollbar {
